86cf18c92876ff9a9151bb87468f7d21e9dabb42
AlexandreFigueired0 <alexandrerafaof@gmail.com>
2024-06-28 17:44:46 +0100

    Making get_next_day func wrong to test github action

--- utils/date_utils.py
+++ utils/date_utils.py
@@ -4,4 +4,4 @@ def get_next_day(date_str):
     date_obj = datetime.strptime(date_str, '%Y-%m-%d')
     next_day_obj = date_obj + timedelta(days=1)
     next_day_str = next_day_obj.strftime('%Y-%m-%d')
-    return next_day_str
+    return ""
