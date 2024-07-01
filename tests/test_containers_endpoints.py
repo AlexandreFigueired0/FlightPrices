ea15719bff8b35c21b49af3d42dad70368188a0e
AlexandreFigueired0 <alexandrerafaof@gmail.com>
2024-07-01 13:29:06 +0100

    added test to the ranking enpoints

--- tests/test_containers_endpoints.py
+++ tests/test_containers_endpoints.py
@@ -14,3 +14,7 @@ def test_get_airline_details(client):
     assert "airlineCode: DL" in response.text
     assert "airlineName: Delta" in response.text
 
+def test_get_ranking(client):
+    response = client.get(f'{BASE_URL}:8082/api/ranking/airlines_by_ticket_price')
+    assert response.status_code == 200
+
