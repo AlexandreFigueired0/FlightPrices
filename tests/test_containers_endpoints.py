c36be83199cab8a627665a68a0c41da8b39ee744
AlexandreFigueired0 <alexandrerafaof@gmail.com>
2024-07-01 12:47:22 +0100

    Fixed the name of the file test_containers_endpoints.py

--- tests/test_containers_endpoints.py
+++ tests/test_containers_endpoints.py
@@ -4,3 +4,4 @@ class TestEndpoints():
     def test_visualization_endpoints(self):
         # run the coomand :curl localhost:8084/api/visualization/airlines/DL
         response = requests.get("localhost:8084/api/visualization/airlines/DL")
+        print(response.json())
