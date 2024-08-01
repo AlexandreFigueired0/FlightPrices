898b4537d10e36a83a3ad49f2c5ecbea80f0ae31
Alexandre Figueiredo <alexandrerafaof@gmail.com>
2024-08-01 17:47:25 +0100

    Tests done for visualization service

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,2 +1,16 @@
+# Create Docker network if not exists
+docker network inspect microservices &>/dev/null || {
+    echo "Creating Docker network..."
+    docker network create microservices
+}
+
+# Startup database services
+cd ../database-ranking
+./run.sh
+
+cd ../database-visualization
+./run.sh
+
+cd ../tests
 docker build -f Dockerfile.test -t test_visualization ..
-docker run -t test_visualization
+docker run -t --network microservices test_visualization 
